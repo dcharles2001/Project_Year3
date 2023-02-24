e0852fda3c7d7cd3332ab66e675b8b9b5a9a8cce
dcharles2001 <daniel.charles@students.plymouth.ac.uk>
2023-02-24 14:39:50 +0000

    Motor Driver Test

--- Project_Year3/main.cpp
+++ Project_Year3/main.cpp
@@ -1,10 +1,32 @@
 #include "mbed.h"
+#include <iostream>
 
-// main() runs in its own thread in the OS
-int main()
+using namespace std;
+
+DigitalOut motorLA(D6);
+DigitalOut motorLB(D5);
+DigitalOut EnableL293D(D7);
+DigitalOut test(D4);
+
+void motorInit()
 {
-    while (true) {
-
-    }
+    motorLA = 0;
+    motorLB = 0;
+    EnableL293D = 1;
 }
 
+void motorForward()
+{
+    motorLA = 1;
+    motorLB = 0;
+    EnableL293D = 1;
+}
+int main()
+{
+    void motorInit();
+    while (true) {
+        void motorForward();
+        test = 1;
+    }
+
+}
